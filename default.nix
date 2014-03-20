fdd66bcca155baeadcdc3a68ae78d7b387c96365
Willem van den Ende <willem@qwan.eu>
2014-03-20 17:12:49 +0000

    Nixos / nixpkg configuration to build and run this project with cabal under nix.
    Allows to run with isolated version of ghc, cabal and packages. The ultimate sandbox?

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ haskellPackages ? ( import <nixpkgs> {}).haskellPackages}:
+let
+  inherit (haskellPackages) cabal cabalInstall_1_18_0_2
+    doctest hashable monadsTf parsec tuple
+ unorderedContainers utf8String ansiTerminal
+ ; # Haskell dependencies here
+
+in cabal.mkDerivation(self: {
+  pname = "hlogic";
+  version = "0.0.1";
+  src = ./.;
+  buildDepends = [
+   # as imported above
+  doctest hashable monadsTf parsec tuple
+ unorderedContainers utf8String ansiTerminal
+ ];
+
+  buildTools = [ cabalInstall_1_18_0_2 ];
+  enableSplitObjs = false;
+})
