dc822970d2e365480ca66b35b1c3f78bc3eb061c
Willem van den Ende <willem@qwan.eu>
2014-03-23 17:31:42 +0000

    Cabal install turns out to be necessary after all
    to check: type which cabal from within nix-shell. If you already had
    cabal installed globally, it refers to that, and not the nix version.
    
    This probably does not work so well trying to find packages

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { haskellPackages ? ( import <nixpkgs> {}).haskellPackages}:
 let
-  inherit (haskellPackages) cabal
+  inherit (haskellPackages) cabal cabalInstall_1_18_0_3
     doctest hashable monadsTf parsec tuple
  unorderedContainers utf8String ansiTerminal
  ; # Haskell dependencies here
@@ -15,5 +15,6 @@ in cabal.mkDerivation(self: {
  unorderedContainers utf8String ansiTerminal
  ];
 
+  buildTools = [ cabalInstall_1_18_0_3 ];
   enableSplitObjs = false;
 })
